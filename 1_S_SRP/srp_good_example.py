fac6394f2770df452d3fe6f9f928d11f1ba59113
Tiago Fernandes <tiagoferprojects@gmail.com>
2024-04-13 18:01:50 -0300

    Resolving Exercise 1

---
+++ 1_S_SRP/srp_good_example.py
@@ -0,0 +1,24 @@
+class API():
+  def connect():
+    pass
+  
+class Notification:
+  def send():
+    pass
+  
+class Report:
+  def generate():
+    pass
+
+  def send():
+    pass
+  
+class TaskHandler:
+  def create():
+    pass
+
+  def update():
+    pass
+
+  def remove():
+    pass
